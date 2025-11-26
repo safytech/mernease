81889eaf91fbb22fdff73d74b1ee6490fd97f329
safytech <mernease@gmail.com>
2025-11-26 19:22:19 +0530

    update

--- server/routes/reset.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import { resetUsers } from "../controllers/reset.controller.js";
-
-const router = express.Router();
-router.get("/reset-users", resetUsers);
-
-export default router;
